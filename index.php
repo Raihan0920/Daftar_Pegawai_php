b9896988651f4efd69f57569863459f9abec2e75
Raihan0920 <4541raihanciro@gmail.com>
2022-10-02 23:42:48 +0700

    first-commit

---
+++ index.php
@@ -0,0 +1,254 @@
+<?php
+require 'index2.php';
+
+// Array Multidimensi
+
+$daftarPegawai = [
+    [
+        'nip' => 521621928,
+        'nama' => 'Andhika oktavianto',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 241892035,
+        'nama' => 'Albaiq Jusian',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 2518240071,
+        'nama' => 'Akhmad David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 2617200167,
+        'nama' => 'Herlan Jayadi',
+        'jabatan' => 'Kabag',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 2619285311,
+        'nama' => 'Kaisar Haijah',
+        'jabatan' => 'Asmen',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 2800192832,
+        'nama' => 'Zahratul Izza',
+        'jabatan' => 'Staff',
+        'agama' => 'islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Baghi Ambarita',
+        'jabatan' => 'Manager',
+        'agama' => 'Konghucu',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 3210982731,
+        'nama' => 'Hariah Saniah Riya',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 391281232,
+        'nama' => 'Sartria Adha',
+        'jabatan' => 'Buddha',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 622190021,
+        'nama' => 'David Oktavianti',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 2519087212,
+        'nama' => 'Idham Syaputra',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 2198750128,
+        'nama' => 'Saniah Marwah',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 2618201809,
+        'nama' => 'Bunga Syifa',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 281023829,
+        'nama' => 'Rayhana Aulia Mawah',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 521828002,
+        'nama' => 'Vidia Azzahra',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 251820102,
+        'nama' => 'larafa Saffa',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 251008290,
+        'nama' => 'Agung Kusaeri',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 281029362,
+        'nama' => 'Satria Arya',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 290821323,
+        'nama' => 'Chummaedi',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 2651027892,
+        'nama' => 'Nuraini Sadir',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 9012387297,
+        'nama' => 'Erlina',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 26810028914,
+        'nama' => 'Ratu Faradiba',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 2519200192,
+        'nama' => 'Ilhamniga',
+        'jabatan' => 'Manager',
+        'agama' => 'Konguchu',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 2710029873,
+        'nama' => 'Farhan',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 2519203662,
+        'nama' => 'Ainul Mega Putri',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 8392001234,
+        'nama' => 'Rizky Idham',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 263911038,
+        'nama' => 'Sofia Arizka',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 638920012,
+        'nama' => 'Andhika Dwiyanto',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 263911038,
+        'nama' => 'Raihan Aditya Pratama Firmansyah',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 265719200,
+        'nama' => 'Bukan Farah dan Tia',
+        'jabatan' => 'Manager',
+        'agama' => 'Hindu',
+        'status' => 'Menikah'
+    ],
+]
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Daftar Pegawai</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container-fluid col-lg-12 bg-light p-4">
+        <h1 class="text-center fs-2 mb-0 my-3"><?= Pegawai::PT ?></h1>
+        <p class="text-center fs-4 mb-5">Menampilkan Data Pegawai Menggunakan Class & Object Class</p>
+
+        <div class="row">
+            <?php
+            // Perulangan
+            foreach ($daftarPegawai as $dpegawai) {
+                $pegawai = new Pegawai($dpegawai['nip'], $dpegawai['nama'], $dpegawai['jabatan'], $dpegawai['agama'], $dpegawai['status']);
+                $pegawai->cetak();
+            }
+            ?>
+        </div><br>
+        <h6 class="text-left p-1 mt-0 my-0 col-lg-3">JUMLAH PEGAWAI: <?= Pegawai::$no ?></h6>
+    </div>
+
+    <p class="text-center text-muted p-5 mt-0 my-0 bg-dark small">@raihanAdit, 2022. All rights reserved</p>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+
+</html>
